847944f2f5a193450fa466193412b0573b4bad43
thisissvhari-1810 <thisissvhari@gmail.com>
2026-06-28 21:35:46 +0530

    added images

--- client/next.config.js
+++ client/next.config.js
@@ -6,14 +6,14 @@ module.exports = {
   reactStrictMode: true,
 
   images: {
-    domains:
-      process.env.NODE_ENV === "production"
-        ? ["quickpics-images.s3.eu-west-2.amazonaws.com"]
-        : [
-            "quickpics-images-test.s3.eu-west-2.amazonaws.com",
-            // for seed data
-            "randomuser.me",
-            "picsum.photos",
-          ],
+    domains: [
+      // seed data (post images + user avatars) - keep these in every env
+      // or the demo posts won't render at all
+      "picsum.photos",
+      "randomuser.me",
+      // original author's S3 buckets (kept so existing prod data still works)
+      "quickpics-images.s3.eu-west-2.amazonaws.com",
+      "quickpics-images-test.s3.eu-west-2.amazonaws.com",
+    ],
   },
 };
